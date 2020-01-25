e84a58beb1917d2c6c113a23cc72ee3fac9821bd
AlexanderHallCS <hallale21@yahoo.com>
2020-01-25 15:39:32 -0600

    Rename file

---
+++ Airline Friend/BranchControllerVC.swift
@@ -0,0 +1,16 @@
+//
+//  BranchControllerVC.swift
+//  Airline Friend
+//
+//  Created by Alexander Hall on 1/25/20.
+//  Copyright © 2020 Hall Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BranchControllerVC: UIViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
