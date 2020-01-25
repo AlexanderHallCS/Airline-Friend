e37cda0a15cb262e672dd3defdf9074f8b3fde71
AlexanderHallCS <hallale21@yahoo.com>
2020-01-25 12:02:45 -0600

    Initial Commit

---
+++ Airline Friend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Airline Friend
+//
+//  Created by Alexander Hall on 1/25/20.
+//  Copyright © 2020 Hall Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
