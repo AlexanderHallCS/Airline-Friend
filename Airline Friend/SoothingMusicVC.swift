d310318077d3e28faf42fc424ca53f2e46169f08
AlexanderHallCS <hallale21@yahoo.com>
2020-01-25 20:33:14 -0600

    Change the button UI on the soothing music VC and add the functioning audio player.

--- Airline Friend/SoothingMusicVC.swift
+++ Airline Friend/SoothingMusicVC.swift
@@ -14,8 +14,53 @@ class SoothingMusicVC: UIViewController {
     
     var audioPlayer = AVAudioPlayer()
     
-    override func viewDidLoad() {
+    @IBAction func playVibesButton(_ sender: UIButton) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "2019-08-09_-_Vibes_-_www.FesliyanStudios.com_-_David_Renda", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+            print("The audio file was not found!")
+        }
         
+        if(audioPlayer.isPlaying == false) {
+            audioPlayer.play()
+        }
+        audioPlayer.numberOfLoops = -1
+    }
+    
+    @IBAction func playStringsOfTimeButton(_ sender: UIButton) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "2016-05-06_-_Strings_of_Time_-_David_Fesliyan", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+            print("The audio file was not found!")
+        }
+        
+        if(audioPlayer.isPlaying == false) {
+            audioPlayer.play()
+        }
+        audioPlayer.numberOfLoops = -1
+    }
+    
+    @IBAction func playTranquilityButton(_ sender: UIButton) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "2019-04-26_-_Tranquility_-_www.fesliyanstudios.com", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+            print("The audio file was not found!")
+        }
+        
+        if(audioPlayer.isPlaying == false) {
+            audioPlayer.play()
+        }
+        audioPlayer.numberOfLoops = -1
+    }
+    
+    @IBAction func stopMusicButton(_ sender: UIButton) {
+        audioPlayer.stop()
     }
     
     @IBAction func backButton(_ sender: UIBarButtonItem) {
